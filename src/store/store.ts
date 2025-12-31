3d4231ecca95f8bc8aed50cd0a94dd13e751a386
Ehsan <ehsan.yaghoti@yahoo.com>
2026-01-01 01:29:06 +0330

    ADD Zustand persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type Store = {
   chats: { id: number; question: string; answer: string }[];
@@ -6,40 +7,43 @@ type Store = {
   insertAnswer: (answerPayload: { id: number; content: string }) => void;
 };
 
-const useChat = create<Store>((set) => ({
-  chats: [],
-  insertQuestion: (question) => {
-    let newId = 0;
-    set((state) => {
-      const lastId = state.chats.length
-        ? state.chats[state.chats.length - 1].id
-        : -1;
+const useChat = create<Store>()(
+  persist(
+    (set) => ({
+      chats: [],
+      insertQuestion: (question) => {
+        let newId = 0;
+        set((state) => {
+          const lastId = state.chats.length
+            ? state.chats[state.chats.length - 1].id
+            : -1;
 
-      newId = lastId + 1;
+          newId = lastId + 1;
 
-      return {
-        chats: [
-          ...state.chats,
-          {
-            id: newId,
-            question,
-            answer: "Loading...",
-          },
-        ],
-      };
-    });
+          return {
+            chats: [
+              ...state.chats,
+              {
+                id: newId,
+                question,
+                answer: "Loading...",
+              },
+            ],
+          };
+        });
 
-    return newId;
-  },
-  insertAnswer: ({ id, content }) => {
-    set((state) => ({
-      chats: state.chats.map((chat) =>
-        chat.id === id
-          ? { ...chat , answer: content }
-          : chat
-      ),
-    }));
-  },
-}));
+        return newId;
+      },
+      insertAnswer: ({ id, content }) => {
+        set((state) => ({
+          chats: state.chats.map((chat) =>
+            chat.id === id ? { ...chat, answer: content } : chat
+          ),
+        }));
+      },
+    }),
+    { name: "chat-storage" }
+  )
+);
 
 export default useChat;
